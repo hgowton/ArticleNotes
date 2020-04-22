925d23349082623eaa7e53f64220dfbaada94af0
hgowton <hgowton@gmail.com>
2020-04-22 16:22:35 -0400

    handlebars views, starter models, built first scrape route in server.js

--- server.js
+++ server.js
@@ -18,6 +18,11 @@ app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({defaultLayout: "main"}));
+app.set("view engine", "handlebars");
+
 //Make public a static folder (for styling)
 app.use(express.static("public"));
 
@@ -25,3 +30,35 @@ app.use(express.static("public"));
 mongoose.connect("mongodb://localhost/articleScraper", {useNewUrlParser: true});
 
 //Routes
+
+//A GET route for scraping BBC website
+app.get("/scrape", function(req,res) {
+    axios.get("https://www.pewresearch.org/topics/news-sources").then(function(response) {
+        var $ = cheerio.load(response.data);
+        $("div.header").each(function(i, element) {
+            var result = {};
+
+            result.title = $(this).children().text();
+            result.link = $(this).find("a").attr("href");
+            // result.image = $(element).children().children().children("img").attr("src");
+        
+            //Create a new Article from the scraping result
+            db.Article.create(result)
+            .then(function(dbArticle) {
+                console.log(dbArticle);
+            })
+            .catch(function(err) {
+                console.log(err);
+            });
+        });
+
+        //Send message letting client know scraping worked
+        res.send("Scrape Complete");
+    });
+});
+
+
+//Start the server
+app.listen(PORT, function() {
+    console.log("App running on port" + PORT + "!");
+})
