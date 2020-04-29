8d95905a9a0005d783b1b060314e8efbc5538b73
hgowton <hgowton@gmail.com>
2020-04-28 22:47:18 -0400

    setting back to server.js

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ var PORT = process.env.PORT || 8080;
 var app = express();
 
 app.use(logger("dev"));
+
 // Parse request body as JSON
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
